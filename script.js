9a219e3455e3cf609b037eec8d2b77a6bc5e1268
Misael Reyes <misaelrey90@gmail.com>
2021-08-05 17:28:19 -0500

    error message added

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-
 function generatePassword() {
-  var letters = "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,z,y,z";
+  var lower = "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,z,y,z";
   var numbers = "0,1,2,3,4,5,6,7,8,9";
   var special = "!,@,#,$,%,^,&,*";
-  var uppper = letters.toUpperCase();
+  var uppper = lower.toUpperCase();
+  let char = "";
   let length = window.prompt("Enter password length:");
-  let char = letters;
+  if (window.confirm("Do you want lowercase letter inside the password")){
+    char = char.concat("," + lower);
+  }
   if (window.confirm("Do you want numbers inside the password")) {
     char = char.concat("," + numbers);
   }
-  if (window.confirm("Do you want uppercase letters in the password")) {
+  if (window.confirm("Do you want uppercase lower in the password")) {
     char = char.concat("," + uppper)
   }
   if (window.confirm("Do you want specail charaters in the password")) {
@@ -20,14 +22,17 @@ function generatePassword() {
   }
   let x = char.split(",");
   let arr = [];
+  console.log(char)
   for (let i = 0; i < parseInt(length); i++) {
     random = Math.floor(Math.random() * x.length)
     arr.unshift(x[random])
+    result = arr.join("");
   }
-  result = arr.join("");
+  if (arr.length === 0 || char === ""){
+    result = "ERROR";
+  }
   return result;
 }
-
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
